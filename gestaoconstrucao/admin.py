49d2d2fae3e32087b6143732e41a23ef94011c08
victorhugo000 <82055949+victorhugo000@users.noreply.github.com>
2021-04-23 19:12:38 -0300

    "adicionando novas class"

--- gestaoconstrucao/admin.py
+++ gestaoconstrucao/admin.py
@@ -1,12 +1,19 @@
 
 from django.contrib import admin
 
-from .models import Venda, Eletricos, Hidraulico, Cliente
-
+from .models import Venda, Eletricos, Hidraulicos, Cliente, Vidros, Tintas, estrutura
+from .models import Pisos, cobertas, ferramentas, iluminacao
 # Register your models here.
 
 admin.site.site_header = 'Gestão de construção'
 admin.site.register(Venda)
 admin.site.register(Eletricos)
 admin.site.register(Cliente)
-admin.site.register(Hidraulico)
+admin.site.register(Hidraulicos)
+admin.site.register(Tintas)
+admin.site.register(Vidros)
+admin.site.register(estrutura)
+admin.site.register(Pisos)
+admin.site.register(cobertas)
+admin.site.register(ferramentas)
+admin.site.register(iluminacao)
--- gestaoconstrucao/models.py
+++ gestaoconstrucao/models.py
@@ -1,45 +1,102 @@
 from django.db import models
 
 class Venda(models.Model):
-    Produtos1 = models.ManyToManyField('Eletricos', verbose_name='Produtos Eletricos')
-    Produtos2 = models.ManyToManyField('Hidraulico', verbose_name='Produtos hidraulicos')
+    numero_venda = models.IntegerField(blank=False, null=False, verbose_name='Número da venda')
+    cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=0)
+
+    ProdutosE = models.ManyToManyField('Eletricos', blank=True, null=True, verbose_name ='Produtos Eletricos')
+    Produtosh = models.ManyToManyField('Hidraulicos', blank=True, null=True, verbose_name ='Produtos hidraulicos')
     valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False,
                                 verbose_name='Valor total da Venda')
+
     data_venda = models.DateField(auto_now_add=True, blank=True, null=False)
     hora_venda = models.TimeField(auto_now_add=True, blank=True, null=False)
     data_hora_venda = models.DateTimeField(auto_now_add=True, blank=True, null=False)
-    numero_venda = models.IntegerField(blank=False, null=False, verbose_name='Número da venda')
-    cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=1)
     observacao = models.TextField(blank=True, null=True, verbose_name='Observação')
     comprovante_venda = models.FileField(blank=True, null=True, verbose_name='Comprovante de venda')
     venda_concluida = models.BooleanField(blank=False, null=False)
 
-
     def __str__(self):
-        return str(self.cliente)
-
-
-class Eletricos(models.Model):
-    nome = models.CharField(max_length=255, blank=False, null=False)
-    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=True)
-
-
-
-    def __str__(self):
-        return self.nome + ' - R$: ' + str(self.valor)
-
-class Hidraulico(models.Model):
-    nome = models.CharField(max_length=255, blank=False, null=False)
-    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
-
-    def __str__(self):
-        return self.nome + '- R$: ' + str(self.valor)
-
-
+        return 'Comprador: ' + str(self.cliente) + ' | Data da venda: ' + str(self.data_venda)
 class Cliente(models.Model):
     nome = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nome')
     cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
     email_cliente = models.EmailField(blank=True, null=True, verbose_name='Email')
 
     def __str__(self):
-        return self.nome
+        return str(self.nome)
+
+class Eletricos(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=True)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + ' - R$: ' + str(self.valor)
+class Hidraulicos(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=True)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + ' - R$: ' + str(self.valor)
+
+class Vidros (models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    cor = models.CharField(max_length=255, blank=False, null=False)
+    espessura = models.CharField(max_length=255, blank=True, null=True, verbose_name='Espessura')
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + ' - ' + str(self.espessura) + ' - R$: ' + str(self.valor)
+class Tintas (models.Model):
+    cor = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    qtd_tintas = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False,
+                                verbose_name='Quantidade de tintas')
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.cor) + '- R$: ' + str(self.valor)
+class estrutura (models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
+class Pisos (models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
+class cobertas (models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    dimensaos = models.CharField(max_length=255, blank=False, null=False, verbose_name='Dimensões')
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
+class ferramentas (models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
+
+
+class iluminacao(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+    ambiente = models.CharField(max_length=255, blank=False, null=False, verbose_name='Em qual ambiente será utilizada?')
+
+    def __str__(self):
+        return str(self.nome) + ' - R$: ' + str(self.valor)
+
+
