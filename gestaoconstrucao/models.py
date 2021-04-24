eff8bea3aa25f9f65efb1d7a5e2b4c6f93f4c5a4
TKZET <tahaktobias@gmail.com>
2021-04-24 15:49:14 -0300

    Adicionando os atributos: Esquadrias e Madeira no Models.py e admin.py. Adicionando os atributos ManytoMany, mesclados a os dois atributos mencionados em Vendas.

--- gestaoconstrucao/models.py
+++ gestaoconstrucao/models.py
@@ -13,6 +13,8 @@ class Venda(models.Model):
     observacao = models.TextField(blank=True, null=True, verbose_name='Observação')
     comprovante_venda = models.FileField(blank=True, null=True, verbose_name='Comprovante de venda')
     venda_concluida = models.BooleanField(blank=False, null=False)
+    Produtosesq = models.ManyToManyField('Esquadria', blank=True, null=True)
+    Produtosmad = models.ManyToManyField('Madeira', blank=True, null=True)
 
 
     def __str__(self):
@@ -60,3 +62,25 @@ class Tinta(models.Model):
 
     def __str__(self):
         return self.nome + '-R$: ' + str(self.valor)
+
+
+class Esquadria(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    material = models.CharField(max_length=255, blank=False, null=False)
+    cor = models.CharField(max_length=255, blank=False, null=False)
+    dimencoes = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
+
+
+class Madeira(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    tipo = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    utiliz = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.nome) + '- R$: ' + str(self.valor)
