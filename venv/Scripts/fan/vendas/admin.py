a144043b5137fc91d24fa2f4ec43838ef45b366d
victorhugo000 <82055949+victorhugo000@users.noreply.github.com>
2021-04-25 12:03:55 -0300

    Delete venv/Scripts/fan directory

--- venv/Scripts/fan/vendas/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import vendas, Produto, Clientes
-# Register your models here.
-
-admin.site.register(vendas)
-admin.site.register(Produto)
-admin.site.register(Clientes)
